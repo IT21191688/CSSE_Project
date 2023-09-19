418ac96564c2f6bf8eabce8569b9deb363e4adbe
IT21191688 <IT21191688@my.sliit.lk>
2023-09-19 16:27:41 +0530

    final

--- frontend/src/screens/AdminHome.js
+++ frontend/src/screens/AdminHome.js
@@ -14,70 +14,6 @@ import { useNavigation } from "@react-navigation/native";
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 const AdminHome = () => {
-    const [firstname, setFirstname] = useState('');
-    const [lastname, setLastname] = useState('');
-    const [email, setEmail] = useState('');
-    const [age, setAge] = useState('');
-    const [dob, setDob] = useState('');
-    const [password, setPassword] = useState('');
-    const role = "user";
-    const navigation = useNavigation();
-
-
-    /*
-    const [clientIP, setClientIP] = useState(null);
-
-    useEffect(() => {
-        // Function to fetch the client's IP address
-        const fetchClientIP = async () => {
-            try {
-                const response = await fetch('http://localhost:8082/getip');
-                if (response.ok) {
-                    const data = await response.json();
-                    setClientIP(data.ip);
-                } else {
-                    console.error('Failed to fetch IP:', response.statusText);
-                }
-            } catch (error) {
-                console.error('Error fetching IP:', error);
-            }
-        };
-
-        // Call the function to fetch the IP address when the component mounts
-        fetchClientIP();
-    }, []);
-
-    */
-
-    const handleRegister = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post(`http://192.168.43.93:8082/auth/register`, {
-                firstname,
-                lastname,
-                email,
-                age,
-                dob,
-                password,
-                role
-            });
-
-            // Check if the response indicates success (you may need to adjust this based on your backend's response format)
-            if (response.status === 200) {
-                console.log('Registration successful:', response.data);
-                // Handle success, e.g., navigate to the next screen
-            }
-        } catch (error) {
-            // Handle network error
-            if (error.message === 'Network Error') {
-                console.error('Network error. Please check your internet connection.');
-                // Handle network error, e.g., show an error message to the user
-            } else {
-                console.error('Registration error:', error);
-                // Handle other types of errors, e.g., show an error message to the user
-            }
-        }
-    };
 
     return (
         <Text>Admin Home</Text>
@@ -91,38 +27,7 @@ const styles = StyleSheet.create({
         alignItems: "center",
         marginTop: 0,
     },
-    logo: {
-        width: 150,
-        height: 100,
-    },
-    inputField: {
-        flexDirection: "row",
-        alignItems: "center",
-        gap: 5,
-        backgroundColor: "#D0D0D0",
-        paddingVertical: 5,
-        borderRadius: 5,
-        marginTop: 15,
-        height: 50,
-        width: 300,
-    },
-    input: {
-        color: "gray",
-        width: "100%",
-        fontSize: 16, // Fixed font size for placeholder
-    },
-    inputIcon: {
-        marginLeft: 10,
-    },
-    registerButton: {
-        width: 200,
-        backgroundColor: "#FEBE10",
-        borderRadius: 6,
-        marginLeft: "auto",
-        marginRight: "auto",
-        padding: 15,
-        marginTop: 30,
-    },
+
 });
 
 export default AdminHome;
